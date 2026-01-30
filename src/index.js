1ebd8656d53dbcba12c6eb602911ca598c555bab
krishu2814 <krishukumarsingh06@gmail.com>
2026-01-30 08:26:09 +0530

    Added User_Roles through table.

--- src/index.js
+++ src/index.js
@@ -3,6 +3,7 @@ const bodyParser = require('body-parser');
 const { PORT } = require('./config/serverConfig');
 const ApiRoutes = require('./routes/index');
 const db = require('./models/index');
+const { User, Role } = require('./models/index');
 
 const startServer = () => {
 
@@ -12,11 +13,22 @@ const startServer = () => {
     app.use(bodyParser.urlencoded({extended: true}));
     app.use('/api', ApiRoutes);
 
-    app.listen(PORT, () => {
+    app.listen(PORT, async () => {
         console.log(`Server is listening at the port ${PORT}`)
-        // if (process.env.DB_SYNC) {
-        //     db.sequelize.sync({ alter: true})
-        // }
+        if (process.env.DB_SYNC) {
+            db.sequelize.sync({ alter: true})
+        }
+
+        /*
+        const u1 = await User.findByPk(4);
+        const u2 = await Role.findByPk(1);
+
+        // u1.addRole(u2); // INSERT INTO `User_Roles`
+        const response = await u2.getUsers();
+        // console.log(u2);
+        console.log("////////////////////");
+        console.log(response);
+        */
     })
 };
 
